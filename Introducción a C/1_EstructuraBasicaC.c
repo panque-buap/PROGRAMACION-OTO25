142344da229696ced638fafc6f97deadefb590dd
unknown <rafeal.garciasan@correo.buap.mx>
2025-09-25 09:01:31 -0600

    Se agrega programa con estructura basica de un programa en C

---
+++ Introducción a C/1_EstructuraBasicaC.c
@@ -0,0 +1,22 @@
+//Todo programa en C, en la parte superior
+//realiza la declaración de bibliotecas
+//(Una biblioteca es un bloque de código
+//que se incluye a nuestro programa)
+
+//#include <stdio.h>
+//#include <stdlib.h>
+
+//Recordemos que en C, colocar dos diagonales
+//representa un comentario de una sola línea
+
+/*En C, tambien podemos usar comentarios de varias
+lineas pero para ello, utilizamos */
+
+
+//A continuación, vamos a colocar
+//la creación de la función principal
+
+int main()
+{
+    return 0;
+}
