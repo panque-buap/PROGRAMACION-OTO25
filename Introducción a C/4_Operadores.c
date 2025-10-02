3766b06e3b5905afea5e323b303acf0ac2fba493
unknown <rafeal.garciasan@correo.buap.mx>
2025-10-02 09:52:42 -0600

    Se actualiza a operadores

---
+++ Introducción a C/4_Operadores.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+//El objetivo de este programa es entender el concepto
+//de operadores en programación
+
+//Un operador es un simbolo o una palabra que representa
+//una operación
+
+//Los operadores se pueden clasificar siendo los más populares:
+//operadores aritmeticos
+//operadores relacionales
+//operadores unitarios
+//operadores a nivel de bits
+//operadores lógicos
+
+int main()
+{
+    //A continuación se muestran los operadores aritmeticos
+
+    //Para entender estos conceptos vamos a crear un programa
+    //que sea capaz de sumar, restar, multiplicar y divir
+    //numeros enteros
+    int num1, num2, result;
+    //Esto es equivalente a
+    //int num1;
+    //int num2;
+    //int result;
+
+    printf("Ingresa el primer numero entero: ");
+    scanf("%d", &num1);
+    printf("\n Ingresa el segundo numero entero: ");
+    scanf("%d", &num2);
+
+    //Vamos ahora a realizar la operación de suma
+    result = num1 + num2;
+    printf("\n El resultado de la suma es: %d \n", result);
+
+    //Vamos ahora a realizar la operación de resta
+    result = num1 - num2;
+    printf("\n El resultado de la resta es: %d \n", result);
+
+    //Vamos ahora a realizar la operación de multiplicación
+    result = num1 * num2;
+    printf("\n El resultado de la multiplicación es: %d \n", result);
+
+    //Vamos ahora a realizar la operación de división
+    result = num1 / num2;
+    printf("\n El resultado de la división es: %d \n", result);
+
+
+
+
+
+    return 0;
+}
